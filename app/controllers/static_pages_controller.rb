42bca487014faf562d617789b0b86a54d07a1f27
Brian <lbrian357@gmail.com>
2017-03-29 02:05:22 -0400

    landing page added

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def resume
+  end
+
+  def blog
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/blog', to: 'static_pages#blog'
+  get '/resume', to: 'static_pages#resume'
 end
